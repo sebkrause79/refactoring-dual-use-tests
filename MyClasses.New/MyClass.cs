e719ef61c7e043f0853caa641ceba9a8b7a67ed8
sebschauer <sebschauer@sebschauer.de>
2024-12-10 13:04:56 +0100

    2. added projects

---
+++ MyClasses.New/MyClass.cs
@@ -0,0 +1,19 @@
+﻿using MyClasses.Old;
+
+namespace MyClasses.New
+{
+    public class MyClass : IMyClass
+    {
+        private IMyClass _vbOriginal;
+
+        public MyClass(int factor)
+        {
+            _vbOriginal = new MyClasses.Old.MyClass(factor);
+        }
+
+        public int Product(int value)
+        {
+            return _vbOriginal.Product(value);
+        }
+    }
+}
