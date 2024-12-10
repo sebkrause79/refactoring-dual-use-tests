ee3f0bc5aeb791a6c16c9ff366b13665d8f91831
sebschauer <sebschauer@sebschauer.de>
2024-12-10 13:16:03 +0100

    4. test structure

---
+++ MyClasses.Tests/MyClassesTests.cs
@@ -0,0 +1,29 @@
+﻿using MyClasses.Old;
+using MyClasses.New;
+using FluentAssertions;
+
+namespace MyClass.Tests
+{
+    public abstract class TestClass
+    {
+        public abstract IMyClass GetTestee(int value);
+
+        [Fact]
+        public void ProductShouldBeInvariantUnderOne()
+        {
+            var testee = GetTestee(1);
+            var value = 42;
+            testee.Product(value).Should().Be(value);
+        }
+    }
+
+    public class VBTestclass : TestClass
+    {
+        public override IMyClass GetTestee(int value) => new MyClasses.Old.MyClass(value);
+    }
+
+    public class CsharpTestclass : TestClass
+    {
+        public override IMyClass GetTestee(int value) => new MyClasses.New.MyClass(value);
+    }
+}
